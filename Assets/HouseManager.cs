f6fc6c2f61e884d3d064d18f17a4a39329f4c679
Matt Grant <teamuba@gmail.com>
2017-05-14 20:39:51 -0600

    House spawning, orthographic view

---
+++ Assets/HouseManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseManager : MonoBehaviour {
+	public GameObject HomeA;
+	public int numSpaces = 6;
+	public int spacesPerRow = 3;
+	private List<GameObject> homes;
+	private int[] spaces;
+
+	// Use this for initialization
+	void Start () {
+		spaces = new int[numSpaces];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// S temp spawn homeA
+		if(Input.GetKeyDown(KeyCode.S)){
+			int space = findOpenSpace();
+			// Each spot is 8 x 6
+			if(space != -1){
+				Vector2 xy = new Vector2((space % spacesPerRow) * 8,
+										  (space / spacesPerRow) * 6);
+				Vector3 pos = new Vector3(xy.x, 0, xy.y);
+				Instantiate(HomeA, pos, Quaternion.identity);
+			}
+		}
+		
+	}
+
+    private int findOpenSpace(){
+        int i = 0;
+		while(i < numSpaces){
+			if(spaces[i] != 1){
+				spaces[i] = 1;
+				return i;
+			} 
+			i++;
+		}
+		return -1;
+    }
+}

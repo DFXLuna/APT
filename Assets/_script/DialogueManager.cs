b98db8560b55646573a5a9281f5f0675f1fc5a35
DFXLuna <teamuba@gmail.com>
2017-07-20 16:57:40 -0600

    Initial dialogue implementation. New bug discovered in adding tenant to back right house when no other houses present

---
+++ Assets/_script/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+	public GameObject canvas;
+	public GameObject textBoxPrefab;
+
+	private GameObject textBox;
+	private GameObject textName;
+	private GameObject textContent;
+
+	void Start () {
+		// Instantiate textbox and get ready to receive text
+		textBox = GameObject.Instantiate(textBoxPrefab, canvas.transform, false);
+		// Gather the name text box and the content text box
+		// There's probably a better way to do the textname getting
+
+		GameObject namePanel;
+		if(!tryGetChildwithTag("NamePanel", textBox, out namePanel)){
+			throw new Exception("Can't find child with tag 'NamePanel'");
+		}
+		if(!tryGetChildwithTag("TextName", namePanel, out textName)){
+			throw new Exception("Can't find child with tag 'TextName'");
+		}
+		
+		if(!tryGetChildwithTag("TextContent", textBox, out textContent)){
+			throw new Exception("Can't find child with tag 'TextContent'");
+		}
+
+		
+	}
+
+
+	private void setText(string t){
+		textContent.GetComponent<Text>().text = t;
+	}
+	private void setName(string t){
+		textName.GetComponent<Text>().text = t;
+	}
+	private bool tryGetChildwithTag(string t, GameObject parent, out GameObject toRet){
+		toRet = null;
+		foreach(Transform child in parent.GetComponentInChildren<Transform>()){
+			if(child.gameObject.tag == t){ 
+				toRet = child.gameObject;
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- TODO.md
+++ TODO.md
@@ -1,4 +1,7 @@
-Fix setting isSaved in persistance manager more than once
+Hook up narrative system to Dialogue scene text box
+
+TenantManager Persistance
+NarrativeManager Persistance
 
 Set/Check triggers for Narrative events
 Migrate persistance to Narrative trigger in Narrative Manager
@@ -6,6 +9,7 @@ Template NarrativeManager getVariable()
 
 Compose save methods in persistance manager
     move isSaved triggers to composed method
+Fix setting isSaved in persistance manager more than once
 
 Fix accessing persistancemanager from persistance
 
